65ebd9b36a40b018a70640bcacaa6f04bbfc8ec9
SlvRodrigues <emerson.rodrigues@outlook.com>
2025-12-09 19:35:20 -0300

    Atualizado

--- hello.py
+++ hello.py
@@ -31,10 +31,12 @@ var1 = 1 #variavel inteira
 var2 = 1.1 #variavel float
 var3 = "Eu sou uma string" #variavel string
 var4 = True #variavel booleana
+var5 = False #variavel booleana
 print(var1)
 print(var2)
 print(var3)
 print(var4)
+print(var5)
 
 x = 2
 y = 3

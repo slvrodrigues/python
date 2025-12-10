d5965fc72197e1cc664beb086ac1012ecc58d167
slvrodrigues <emerson.rodrigues@outlook.com>
2025-12-10 15:41:56 -0300

    strings parte 2

---
+++ strings02.py
@@ -0,0 +1,12 @@
+#Strings com metodos string = string.metodo()
+
+a="Diego"
+b="Maradona"
+
+concatenar = a + " "+ b + "\n"
+print(concatenar.strip()) #funcão que remove o espaco
+concatenar = concatenar.upper()
+print(concatenar)
+
+#print(concatenar.lower())
+#print(concatenar.upper())

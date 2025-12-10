182d7aa47d41d5d761792825ffb814c498fff8bf
slvrodrigues <emerson.rodrigues@outlook.com>
2025-12-10 14:50:16 -0300

    Strings parte1

---
+++ strings.py
@@ -0,0 +1,21 @@
+#var1 = "1" declaradas entre aspas simples ou duplas
+#var2 = '2'
+a = "Diego"
+b = "Mariano"
+concatenar = a +" "+b
+print(concatenar) 
+
+#função len saber tamanho da string
+tamanho = len(concatenar)
+print(tamanho)
+
+print(a[0]) #imprimir posição exata do caractere
+#print(a[1])
+print(a[2])
+#print(a[3])
+print(a[4])
+
+c = "Carlos"
+d = "Alberto"
+concatenar = c + " "+ d #imprime o trecho da string 
+print(concatenar[0:6])

8ad34012f549a37fe00a70c078e7ae9c44bc5850
slvrodrigues <emerson.rodrigues@outlook.com>
2025-12-10 13:14:50 -0300

    estrutura for

---
+++ for.py
@@ -0,0 +1,10 @@
+lista1 = [1,2,3,4,5]
+lista2 = ["a","b","c","d","e"]
+lista3 = [0,"um","2",3,"quatro"]
+
+for i in lista1:
+    print(i)
+for i in lista2:
+    print(i)
+for i in lista3:
+    print(i)
--- while.py
+++ while.py
@@ -5,9 +5,9 @@ while x < 10:
 x = 1
 while x < 10:
     print(x)
-    x = x+2
+    x = x + 2
 
 x = 1
 while x < 10:
     print(x)
-    x +=1 # x = x +1
+    x += 1 # x = x +1

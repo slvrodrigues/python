0fec485ebf2bc84e1cc21c465cf2fca9596ca813
SlvRodrigues <emerson.rodrigues@outlook.com>
2025-12-14 18:22:46 -0300

    Ok

---
+++ dicionarios.py
@@ -0,0 +1,5 @@
+#dicionario = {'CHAVE': 'valor1'}
+meu_dicionario = {'A':'AMEIXA', 'B':'BOLA', 'C':'CAVALO'}
+print(meu_dicionario['A']) #acessa o valor da chave A
+for chave in meu_dicionario:
+    print(chave+"-"+meu_dicionario[chave]) #itera sobre as chaves do dicionario

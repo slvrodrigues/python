7a5c2d85fa32f67eaf8c3c5cd0f6e899c09566d2
SlvRodrigues <emerson.rodrigues@outlook.com>
2025-12-14 18:25:44 -0300

    ok

---
+++ listas2.py
@@ -0,0 +1,11 @@
+lista = [124,345,5,72,46,6,7,3,1,7,0]
+lista2=['banana','abacaxi','laranja','uva','manga']
+
+lista.sort() #ordena a lista em ordem crescente
+print(lista)
+lista.sort(reverse=True) #ordena a lista em ordem decrescente
+print(lista)
+#lista.reverse() inverte a ordem da lista
+print(lista)
+lista2.sort()
+print(lista2)
